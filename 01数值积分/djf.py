aa23ba82c6c93d48cc9f4c5df655606c881041d0
xiaoyan94 <ruguo_0904@163.com>
2016-10-11 21:58:33 +0800

    1

---
+++ 01数值积分/djf.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+def djf(a,b,f,delta):#参数依次为积分下限、积分上限、被积函数、精度（delta x）
+	x = a
+	s = 0
+	while x<=b:
+		x = x + delta
+		s = s + f(x)*delta
+	return s
+
+for delta in [0.1,0.01,0.001,0.0001,0.00001,0.000001]:
+	print u"函数f(x)=x从0到10积分，\t\t精度为%f时近似值："%delta,djf(0,10,lambda x: x,delta)
+for delta in [0.1,0.01,0.001,0.0001,0.00001,0.000001]:
+	print u"函数f(x)=x^2从0到10积分，\t精度为%f时近似值："%delta,djf(0,10,lambda x: x**2,delta)
